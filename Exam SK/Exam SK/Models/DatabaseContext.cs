810e036cc47a319bf851b6c379f9d72e8bf7270a
Shuajb Jahija <sj27162@seeu.edu.mk>
2019-11-28 09:07:20 +0100

    initial commit

---
+++ Exam SK/Exam SK/Models/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam_SK.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=rd27240EX01;Database=DB02;Trusted_Connection=True;");
+        }
+
+    }
+}
